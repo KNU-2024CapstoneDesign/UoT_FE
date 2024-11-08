2d4d666e82555f761937af75bf6d6fc33ea1a3de
Seunghyun Ko <chris00825@naver.com>
2024-11-09 01:01:55 +0900

    style: modify indent

--- src/pages/Holder/Home/HolderHome.tsx
+++ src/pages/Holder/Home/HolderHome.tsx
@@ -11,30 +11,30 @@ import {
 
 export const HolderHomePage = () => {
   return  (
-      <Wrapper>
-        <TitleText fontSize='3x1' as='b'>
-          증명서를 발급받거나 기존 증명서를 확인하세요
+    <Wrapper>
+      <TitleText fontSize='3x1' as='b'>
+        증명서를 발급받거나 기존 증명서를 확인하세요
         </TitleText>
-        <TypeWrapper>
-        <Link to={RouterPath.requestVc}>
+      <TypeWrapper>
+      <Link to={RouterPath.requestVc}>
+        <CardWrapper>
+          <CardBody>
+              <CardTitle>
+                증명서 발급
+              </CardTitle>
+            </CardBody>
+          </CardWrapper>
+        </Link>
+        <Link to={RouterPath.myVc}>
           <CardWrapper>
-              <CardBody>
-                <CardTitle>
-                  증명서 발급
-                </CardTitle>
-              </CardBody>
-            </CardWrapper>
-          </Link>
-          <Link to={RouterPath.myVc}>
-            <CardWrapper>
-              <CardBody>
-                <CardTitle>
-                  발급받은 증명서 관리
-                </CardTitle>
-              </CardBody>
-            </CardWrapper>
-          </Link>
-        </TypeWrapper>
-      </Wrapper>
+            <CardBody>
+              <CardTitle>
+                발급받은 증명서 관리
+              </CardTitle>
+            </CardBody>
+          </CardWrapper>
+        </Link>
+      </TypeWrapper>
+    </Wrapper>
   );
 };
